e8d2a785b639f8675d1e0c2f9b373720dbde6478
Guillermo <grlara9@gmail.com>
2022-04-08 00:12:42 -0500

    create a database to start working with routes on authentication

--- controllers/goalControllers.js
+++ controllers/goalControllers.js
@@ -1,22 +1,24 @@
-const getGoals = (req, res) =>{
-    res.status(200).json({ message: 'get goals' })
-}
+const asyncHandler = require('express-async-handler')
 
-const setGoals = (req, res)=> {
+const getGoals = asyncHandler(async (req, res) =>{
+    res.status(200).json({ message: 'get goals' })
+})
+
+const setGoals = asyncHandler(async (req, res)=> {
    if(!req.body.text){
        res.status(400)
        throw new Error('please add a text field')
    }
     res.status(200).json({ message: 'set goals' })
-}
+})
 
-const updateGoal = (req, res)=>{
+const updateGoal = asyncHandler( async(req, res)=>{
     res.status(200).json({ message: `update goal ${req.params.id}`  })
-}
+})
 
-const deleteGoal = (req, res)=>{
+const deleteGoal = asyncHandler( async(req, res)=>{
     res.status(200).json({ message: `delete goal ${req.params.id}`  })
-}
+})
 module.exports = 
 {
     getGoals,

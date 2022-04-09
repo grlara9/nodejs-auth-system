edbe52b467c6e79e3851703813fce27e446bd8da
Guillermo <grlara9@gmail.com>
2022-04-09 00:38:28 -0500

    create a database using mongodb atlas and create connectio file

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const { default: mongoose } = require('mongoose')
+const moongose = require('mongoose')
+
+const connectDB = async () =>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Mongo connected: ${conn.connection.host}` )
+
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB

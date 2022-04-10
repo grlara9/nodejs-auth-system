08ea2760988657da19ab292a8a85d85cf30d96d0
Guillermo <grlara9@gmail.com>
2022-04-10 02:49:33 -0500

    connect app to the database

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
-const { default: mongoose } = require('mongoose')
-const moongose = require('mongoose')
+const mongoose = require('mongoose')
 
 const connectDB = async () =>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`Mongo connected: ${conn.connection.host}` )
+        console.log(`Mongo connected: ${conn.connection}` )
 
     } catch (error) {
         console.log(error)

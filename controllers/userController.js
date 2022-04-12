57336a8fee9caa9e1a165ace7c34241aacecacec
Guillermo <grlara9@gmail.com>
2022-04-12 00:25:57 -0500

    create three users routes, login and register, tested all routes

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+const getUsers = asyncHandler(async(req, res) =>{
+   
+
+    res.status(200).json({message: 'get users'})
+})
+
+const loginUser = asyncHandler(async(req, res)=>{
+   
+     res.status(200).json({message: 'login user success'})
+})
+
+const registerUser = asyncHandler(async(req, res)=>{
+    res.status(200).json({message: 'register users success'})
+})
+
+module.exports = 
+{
+    getUsers,
+    loginUser,
+    registerUser,
+}

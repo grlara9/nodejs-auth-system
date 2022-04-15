1a9e06d0ea7e2f8d097092b494f37afe1dbd6e23
Guillermo <grlara9@gmail.com>
2022-04-14 23:40:15 -0500

    hash password and create user on database using postman, testing routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,7 +20,35 @@ const registerUser = asyncHandler(async(req, res)=>{
         res.status(400)
         throw new Error('all fields must be added')
     }
-    res.status(200).json({message: 'register users success'})
+
+    const userExist = await User.findOne({email})
+
+    if(userExist){
+        res.status(400)
+        throw new Error('User already exist')
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+      })
+
+      if (user) {
+        res.status(201).json({
+          _id: user.id,
+          name: user.name,
+          email: user.email,
+         
+        })
+      } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+      }
 })
 
 module.exports = 

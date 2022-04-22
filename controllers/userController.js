60d6e2989bab7d2bca7a7227192a3425c6a69474
Guillermo <grlara9@gmail.com>
2022-04-22 01:25:12 -0500

    test register route to see if token is created , and then try to log in

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,6 +59,7 @@ const registerUser = asyncHandler(async(req, res)=>{
           _id: user.id,
           name: user.name,
           email: user.email,
+          token: generateToken(user._id)
          
         })
       } else {
